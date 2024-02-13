61fc20886219e2315b5d5d929c6002388b010a2b
Prince211105 <princepatel211103@gmail.com>
2024-02-13 22:38:29 +0530

    container config done

--- First/src/Part1/Allpart1.jsx
+++ First/src/Part1/Allpart1.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import Component from './Component'
-import Jsx from './Jsx'
-import Propscomponent from './Propscomponent'
-import Renderobj from './Rederobj'
-import Task1 from './Task1'
-import Task2 from './Task2'
+import React from "react";
+import Component from "./Component";
+import Jsx from "./Jsx";
+import Propscomponent from "./Propscomponent";
+import Renderobj from "./Rederobj";
+import Task1 from "./Task1";
+import Task2 from "./Task2";
+import Comp from './Comp'
+import Table from "./Table";
+import Bobject from "./Bobject";
+import Function from "./Function";
+import Task3 from "./Task3";
+import Task4 from "./Task4";
+import Task5 from "./Task5";
+import Bect from './Bect';
+import Classes from "./Classes";
+import Pratices from './Pratices'
 function Allpart1() {
   return (
     <div>
-        {/* <Component/> */}
-        {/* <Jsx/> */}
-        {/* <Propscomponent/> */}
-        {/* <Renderobj/> */}
-        {/* <Task1/> */}
-        <Task2/>
+      {/* <Component/> */}
+      {/* <Jsx/> */}
+      {/* <Propscomponent/> */}
+      {/* <Renderobj/> */}
+      {/* <Task1/> */}
+      {/* <Task2/> */}
+      {/* <Comp/> */}
+      {/* <Table/> */}
+      {/* <Bobject/> */}
+      {/* <Function/> */}
+      {/* <Task3/>
+      <Task4/>
+      <Task5/> */}
+      {/* <Bect/> */}
+      {/* <Classes/> */}
+      <Pratices/>
     </div>
-  )
+  );
 }
 
-export default Allpart1
+export default Allpart1;
