e96f8691bc08f498a50d4847392c35308eec3218
Prince211105 <princepatel211103@gmail.com>
2024-02-22 19:45:58 +0530

    container config

--- First/src/Part2/Allpart2.jsx
+++ First/src/Part2/Allpart2.jsx
@@ -1,38 +1,68 @@
-import React from 'react'
-import Rendering from './Rendering'
-import Redata from './Redata'
-import Key from './Key'
-import Index from './Index'
-import Refactoring from './Refactoring'
-import Refactoringdata from './Refactoringdata'
-import Task1 from './Task1'
-import Task2 from './Task2'
-import Task3 from './Task3'
-import Task4 from './Task4'
-import Task5data from './Task5data'
-import Storingdata from './Storingdata'
-import Controll from './Controll'
-import Controlldata from './Controlldata'
-import Filteringdata from './Filteringdata'
-import Pratices from './Pratices'
+import React from "react";
+import Rendering from "./Rendering";
+import Redata from "./Redata";
+import Key from "./Key";
+import Index from "./Index";
+import Refactoring from "./Refactoring";
+import Refactoringdata from "./Refactoringdata";
+import Task1 from "./Task1";
+import Task2 from "./Task2";
+import Task3 from "./Task3";
+import Task4 from "./Task4";
+import Task5data from "./Task5data";
+import Storingdata from "./Storingdata";
+import Controll from "./Controll";
+import Controlldata from "./Controlldata";
+import Filteringdata from "./Filteringdata";
+import Pratices from "./Pratices";
+import Praticesdata from "./Praticesdata";
+import Task6 from "./Task6";
+import Task7 from "./Task7";
+import Task8 from "./Task8";
+import Task9 from "./Task9";
+import Task10 from "./Task10";
+import Datsshow from "./Datsshow";
+import ShowData from "./ShowData";
+import Task11 from "./Task11";
+import Sendingdata from "./Sendingdata";
+import Changenote from "./Changenote";
+import PostData from "./PostData";
+import Task14 from "./Task14";
+import Task15 from "./Task15";
+import Task12 from './Task12'
 function Allpart2() {
   return (
     <div>
-        {/* <Rendering/> */}
-        {/* <Kex */}
-        {/* <Index/> */}
-        {/* <Refactoringdata/> */}
-        {/* <Task1/>
+      {/* <Rendering/> */}
+      {/* <Kex */}
+      {/* <Index/> */}
+      {/* <Refactoringdata/> */}
+      {/* <Task1/>
         <Task2/>
         <Task3/> */}
-        {/* <Task4/> */}
-        {/* <Task5data/> */}
-        {/* <Storingdata/> */}
-        {/* <Controlldata/> */}
-        {/* <Filteringdata/> */}
-        <Pratices/>
+      {/* <Task4/> */}
+      {/* <Task5data/> */}
+      {/* <Storingdata/> */}
+      {/* <Controlldata/> */}
+      {/* <Filteringdata/> */}
+      {/* <Pratices/> */}
+      {/* <Praticesdata/> */}
+      {/* <Task6/>
+        <Task7/>
+        <Task8/> */}
+      {/* <Task9/> */}
+      {/* <Task10/> */}
+      {/* <Datsshow/> */}
+      {/* <ShowData/> */}
+      {/* <Task11 /> */}
+      {/* <Sendingdata/> */}
+      {/* <Changenote/> */}
+      {/* <PostData/> */}
+      {/* <Task14/> */}
+      {/* <Task15/> */}
+      <Task12/>
     </div>
-  )
+  );
 }
 
-export default Allpart2
+export default Allpart2;
